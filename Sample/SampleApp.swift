e0d4ca0804cf035240951053059c05a9a4312ca0
Sarathraj Srinivasan <sarathsharp@gmail.com>
2024-08-22 12:58:54 -0500

    test commit

---
+++ Sample/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Sample
+//
+//  Created by Sarathraj Srinivasan on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
